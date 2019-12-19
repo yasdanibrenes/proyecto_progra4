3b70e831e1c49ed6ee7c693f4b991a34b6cb61e7
YASDANI BRENES VEGA <yasdanibrenes@gmail.com>
2019-12-19 09:26:57 -0600

    Update footer.php

--- Views/footer.php
+++ Views/footer.php
@@ -1,36 +1,31 @@
 <footer class="pt-4 my-md-5 pt-md-5 border-top">
             <div class="row">
             <div class="col-12 col-md">
-                <img class="mb-2" src="./src/logo.png" alt="" width="72" height="72">
+                <img class="mb-2" src="./src/logo.svg" alt="" width="90" height="90">
                 <small class="d-block mb-3 text-muted">&copy; 2019 Wheels LLC</small>
             </div>
             <div class="col-6 col-md">
-                <h5>Features</h5>
+                <h5>Pages</h5>
                 <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">Cool stuff</a></li>
-                <li><a class="text-muted" href="#">Random feature</a></li>
-                <li><a class="text-muted" href="#">Team feature</a></li>
-                <li><a class="text-muted" href="#">Stuff for developers</a></li>
-                <li><a class="text-muted" href="#">Another one</a></li>
-                <li><a class="text-muted" href="#">Last time</a></li>
+                <li><a class="text-muted" href="index.php">Home</a></li>
+                <li><a class="text-muted" href="services.php">Services</a></li>
+                <li><a class="text-muted" href="products.php">Products</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md">
+                <h5>Pages</h5>
+                <ul class="list-unstyled text-small">
+                    <li><a class="text-muted" href="about.php">About us</a></li>
+                    <li><a class="text-muted" href="contact.php">Contact Us</a></li>
+                    <li><a class="text-muted" href="support.php">Support</a></li>
                 </ul>
             </div>
             <div class="col-6 col-md">
                 <h5>Resources</h5>
                 <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">Resource</a></li>
-                <li><a class="text-muted" href="#">Resource name</a></li>
-                <li><a class="text-muted" href="#">Another resource</a></li>
-                <li><a class="text-muted" href="#">Final resource</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>About</h5>
-                <ul class="list-unstyled text-small">
-                <li><a class="text-muted" href="#">Team</a></li>
-                <li><a class="text-muted" href="#">Locations</a></li>
-                <li><a class="text-muted" href="#">Privacy</a></li>
-                <li><a class="text-muted" href="#">Terms</a></li>
+                    <li><a class="text-muted" href="#">Privacy</a></li>
+                    <li><a class="text-muted" href="#">Terms</a></li>
+                    <li><a class="text-muted" href="login.php">Login</a></li>
                 </ul>
             </div>
             </div>
